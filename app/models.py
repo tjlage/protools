5983a21021b5a40aef7dd5b9b1c01d671b66e686
tjlage <tjlage1@gmail.com>
2015-12-10 16:32:05 -0500

    CSV import without alowing Duplicates

--- app/models.py
+++ app/models.py
@@ -4,10 +4,13 @@ from django.utils import timezone
 # Create your models here.
 
 class IP(models.Model):
+    date = models.CharField("Date", max_length=15)
     ip = models.GenericIPAddressField("IP")
     site = models.CharField("Site", max_length=50)
-    tunn = models.CharField("Tunnel", max_length=50)
+    tunName = models.CharField("Tunnel Name", max_length=50)
+    tunType = models.CharField("Tunnel Type", max_length=3)
+    reach = models.CharField("Reachable", max_length=5)
 
 class Ping(models.Model):
-    reach = models.BooleanField(default=False)
+    #reach = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=timezone.now)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpRequest, HttpResponse
-from app.models import *
-
+from app.models import IP
+from django.core.exceptions import ObjectDoesNotExist
+import csv
 # Create your views here.
 def sites(request):
     #return HttpResponse('<html><head><title>Hello, Django!</title></head><body><h1>Hello, Django</h1></body></html>')
@@ -10,3 +11,28 @@ def sites(request):
 
 def home(request):
     return HttpResponse('<html><head><title>Home!</title></head><body><h1>Home Sweet Home</h1></body></html>')
+
+def ping(request):
+    dataReader = csv.reader(open("app/result/ping sample.csv"), delimiter=',')
+
+    for row in dataReader:
+        if row[0] != 'Date/Time': # Ignore the header row, import everything else
+            ping = IP()
+            ping.date = row[0]
+            ping.site = row[1]
+            ping.tunName = row[2]
+            ping.tunType = row[3]
+            ping.ip = row[4]
+            ping.reach = row[5]
+            
+            dtexists = IP.objects.filter(date__iexact = ping.date).exists()
+            diexists = IP.objects.filter(ip__iexact = ping.ip).exists()
+
+            if not diexists:
+                    ping.save()
+            else:
+                pass
+
+        pings = IP.objects.all()
+
+    return render_to_response('app/ping.html', { 'pings' : pings })
